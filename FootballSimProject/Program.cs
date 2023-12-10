6d9251a7090aa9025a4f070f18480633f3d8979c
dradwul <thelu@DESKTOP-A21NA03>
2023-12-10 09:22:17 +0100

    Kommentar om vad som ska finnas initialt

--- FootballSimProject/Program.cs
+++ FootballSimProject/Program.cs
@@ -1,7 +1,14 @@
 ﻿namespace FootballSimProject
 {
+    //-Initialt, vad ska finnas?
+    // 	+Möjlighet att skapa serie
+    // 	+Möjlighet att lägga till lag
+    // 	+Möjlighet att lägga till spelare med stats
+    // 	+Statsen på spelarna i laget ska lägga grund för
+    // 	 hur sannolikt det är att ett lag ska vinna/förlora
     internal class Program
     {
+        
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
