b0732c10fbd1d6c5621460255e0240c8988750e0
dradwul <thelu@DESKTOP-A21NA03>
2023-12-10 09:14:40 +0100

    first commit

---
+++ FootballSimProject/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace FootballSimProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
